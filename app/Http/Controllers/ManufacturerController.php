6f5f6c8692f3d4e96b6225aa799021bf1dd5ad69
Alec Gordon <insanealec@gmail.com>
2024-03-09 09:09:35 -0500

    Easy peasy, copilot is really nice

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -8,61 +8,68 @@ use Inertia\Inertia;
 
 class ManufacturerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-      return Inertia::render('RoofMaxx/Manufacturers', [
-        'manufacturers' => Manufacturer::all()
-      ]);
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return Inertia::render('RoofMaxx/Manufacturers', [
+      'manufacturers' => Manufacturer::all()
+    ]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return Inertia::render('RoofMaxx/CreateManufacturer');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    Manufacturer::create($request->all());
+    return redirect()->route('manufacturers.index');
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Manufacturer $manufacturer)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Manufacturer $manufacturer)
+  {
+    return Inertia::render('RoofMaxx/Manufacturer', [
+      'Manufacturer' => $manufacturer
+    ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Manufacturer $manufacturer)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Manufacturer $manufacturer)
+  {
+    return Inertia::render('RoofMaxx/EditManufacturer', [
+      'Manufacturer' => $manufacturer
+    ]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Manufacturer $manufacturer)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Manufacturer $manufacturer)
+  {
+    $manufacturer->update($request->all());
+    return redirect()->route('manufacturers.index');
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Manufacturer $manufacturer)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Manufacturer $manufacturer)
+  {
+    $manufacturer->delete();
+    return redirect()->route('manufacturers.index');
+  }
 }
