658bb7309994b6cd1c28252d70c5719de12ff1ac
Alec Gordon <insanealec@gmail.com>
2024-03-09 08:22:44 -0500

    TDD for belongsTo relationship

--- tests/Unit/CarTest.php
+++ tests/Unit/CarTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Models\Car;
+use App\Models\Manufacturer;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,5 +18,22 @@ class CarTest extends TestCase
         $car->name = $this->faker->name;
 
         $this->assertTrue($car->save());
+    }
+
+    public function test_car_has_manufacturer(): void
+    {
+        $manufactuer = new Manufacturer;
+        $manufactuer->name = $this->faker->name;
+        $this->assertTrue($manufactuer->save());
+
+        $car = new Car;
+        $car->name = $this->faker->name;
+        $this->assertTrue($car->save());
+
+        $this->assertNull($car->manufacturer);
+
+        $car->manufacturer_id = $manufactuer->id;
+        $this->assertTrue($car->save());
+        $this->assertNotNull($car->manufacturer);
     }
 }
