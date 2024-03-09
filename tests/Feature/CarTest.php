8d22bc10aca018d3fc4bdb310f0beee02cd8fbce
Alec Gordon <insanealec@gmail.com>
2024-03-08 21:03:06 -0500

    TDD - test every resource function and actually name the function right (test prefix)

--- tests/Feature/CarTest.php
+++ tests/Feature/CarTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CarTest extends TestCase
@@ -11,10 +10,59 @@ class CarTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function can_view_cars_page(): void
+    public function test_can_view_cars_page(): void
     {
         $response = $this->get('/cars');
 
         $response->assertStatus(200);
+    }
+
+    public function test_can_view_car_creation_page(): void
+
+    {
+        $response = $this->get('/cars/create');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_can_store_car(): void
+    {
+        $response = $this->post('/cars', [
+            'name' => 'Test Car',
+            'manufacturer_id' => null
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_can_show_car(): void
+    {
+        $response = $this->get('/cars/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_can_view_car_edit_page(): void
+    {
+        $response = $this->get('/cars/1/edit');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_can_update_car(): void
+    {
+        $response = $this->put('/cars/1', [
+            'name' => 'Test Car',
+            'manufacturer_id' => null
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_can_delete_car(): void
+    {
+        $response = $this->delete('/cars/1');
+
+        $response->assertStatus(302);
     }
 }
