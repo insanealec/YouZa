b581925b104c2360a2f5d43ad31f0e2ed4f93747
Alec Gordon <insanealec@gmail.com>
2024-03-09 09:07:34 -0500

    TDD for Manufacturer Resource Controller

--- tests/Feature/ManufacturerTest.php
+++ tests/Feature/ManufacturerTest.php
@@ -2,17 +2,79 @@
 
 namespace Tests\Feature;
 
+use App\Models\Manufacturer;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ManufacturerTest extends TestCase
 {
-    use DatabaseMigrations;
+  use DatabaseMigrations, WithFaker;
 
-    public function test_can_view_manufacturers_page(): void
-    {
-        $response = $this->get('/manufacturers');
+  public function setUp(): void
+  {
+      parent::setUp();
+      $manufacturer = new Manufacturer;
+      $manufacturer->name = $this->faker->name;
+      $manufacturer->save();
+  }
 
-        $response->assertStatus(200);
-    }
+  public function test_can_view_manufacturers_page(): void
+  {
+      $response = $this->get('/manufacturers');
+
+      $response->assertStatus(200);
+  }
+
+  public function test_can_view_manufacturer_creation_page(): void
+  {
+      $response = $this->get('/manufacturers/create');
+
+      $response->assertStatus(200);
+  }
+
+  public function test_can_store_manufacturer(): void
+  {
+      $response = $this->post('/manufacturers', [
+          'name' => 'Test Manufacturer',
+      ]);
+
+      $response
+          ->assertSessionHasNoErrors()
+          ->assertRedirect('/manufacturers');
+  }
+
+  public function test_can_show_manufacturer(): void
+  {
+      $response = $this->get('/manufacturers/1');
+
+      $response->assertStatus(200);
+  }
+
+  public function test_can_view_manufacturer_edit_page(): void
+  {
+      $response = $this->get('/manufacturers/1/edit');
+
+      $response->assertStatus(200);
+  }
+
+  public function test_can_update_manufacturer(): void
+  {
+      $response = $this->put('/manufacturers/1', [
+          'name' => 'Test Manufacturer',
+      ]);
+
+      $response
+          ->assertSessionHasNoErrors()
+          ->assertRedirect('/manufacturers');
+  }
+
+  public function test_can_delete_manufacturer(): void
+  {
+      $response = $this->delete('/manufacturers/1');
+
+      $response
+          ->assertSessionHasNoErrors()
+          ->assertRedirect('/manufacturers');
+  }
 }
